129a1289e94cddf14224af4029d4e813dddc734c
mugisha636 <mugisha134emmy@gmail.com>
2023-04-15 16:52:37 +0200

    production

--- src/config/config.js
+++ src/config/config.js
@@ -8,5 +8,11 @@ module.exports = {
         "host": process.env.DEV_DB_HOST,
         "dialect": "postgres"
       },
-      
+      production: {
+        use_env_variable: 'PROD_URL', // postgres  DEV_DB_USERNAME
+        "password": process.env.DEV_DB_PASSWORD,
+        "database": process.env.DEV_DB_NAME,
+        "host": process.env.DEV_DB_HOST,
+        "dialect": "postgres"
+      },
 }
